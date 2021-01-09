c3e7e10a4976e2fefd59bb66795974f8f3f5f59b
ereksonbrenda <ere18002@byui.edu>
2021-01-09 13:05:38 -0700

    time hard

--- js/home.js
+++ js/home.js
@@ -1,2 +1,2 @@
-const options{weekday: 'long', day:'numeric', month:'long', year:'numeric' };
+const options {weekday: 'long', day:'numeric', month:'long', year:'numeric' };
       document.getElementById=("currentDate").textContent=new Date().toLocaleDateString('en-US',options);
